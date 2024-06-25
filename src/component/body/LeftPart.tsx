48c0f129f9ab49201ed6e8aa5ccfc71960088deb
yassine <yassine.n13@gmail.com>
2024-06-25 19:21:18 +0200

    start left part

--- src/component/body/LeftPart.tsx
+++ src/component/body/LeftPart.tsx
@@ -1,5 +1,7 @@
+import { Rules } from "../leftPart/Rules";
+
 export function LeftPart() {
-  return <div className="w-1/2">
-    
+  return <div className="w-1/2 flex flex-col items-center">
+    <Rules/>
   </div>
 }
---
+++ src/component/leftPart/Rules.tsx
@@ -0,0 +1,5 @@
+export function Rules() {
+  return <div className="bg-pink-200 w-5/6 h-1/5 rounded-3xl">
+    <h2 className="uppercase text-md text-center font-bold">Rules</h2>
+  </div>
+}
