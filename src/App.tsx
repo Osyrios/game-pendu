fc58fdf99c105dc6982a42b4b5ef955c4105d6f1
yassine <yassine.n13@gmail.com>
2024-06-25 07:01:31 +0200

    commit initial du projet

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+
+  function name(a:string, b:string) {
+    let nameString:string = a + ' ' + b
+    console.log(nameString) 
+  }
+  
+  name('yassine', 'test')
+
+  return (
+    <>
+      <h1></h1>
+    </>
+  )
+}
+
+export default App
