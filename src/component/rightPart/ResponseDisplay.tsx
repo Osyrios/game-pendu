6211307584696cd2e9516cc1ca626adaa71aa174
yassine <yassine.n13@gmail.com>
2024-10-08 17:53:26 +0200

    random

--- src/component/rightPart/ResponseDisplay.tsx
+++ src/component/rightPart/ResponseDisplay.tsx
@@ -4,31 +4,49 @@ import { useEffect, useState } from "react"
 
 export function ResponseDisplay() {
 
-    const [randomWord, setRandomWord] = useState('')
-    const [category, setCategory] = useState('')
-
-    const [responseToFind, setResponseToFind] = useState(randomWord.toString().toUpperCase().split(''))
-    const [responseToDisplay, setResponseToDisplay] = useState(responseToFind.map((letter)=>letter='-'))
+  const [randomWord, setRandomWord] = useState('')
+  //const [category, setCategory] = useState('')
+  const [responseToDisplay, setResponseToDisplay] = useState([''])
 
     /**
      * fonction qui génère un mot aléatoire et l'applique a randomWord et category
      */
     async function newRandomWord() {
-      const response = await fetch("https://trouve-mot.fr/api/random", {
-        method: 'GET',
-        headers: {
-          accept:'application-json'
-        }
-      })
-      const bodyResponse = await response.json()
-      setCategory(bodyResponse[0].categorie)
+    const response = await fetch("https://trouve-mot.fr/api/random", {
+    method: 'GET',
+    headers: {
+    accept:'application-json'
+    }
+    })
+    const bodyResponse = await response.json()
+    //setCategory(bodyResponse[0].categorie)
       setRandomWord(bodyResponse[0].name)
+
     } 
-  
-  useEffect(() => {
-      newRandomWord()
-  }, [])
-  
+
+
+    /**
+     * fonction qui remplace les lettre en tiret pour cacher la réponse
+     * @param {string} word 
+     * @returns {string[]}
+     */
+    function hideResponse(word:string) {
+    const wordAsArray = word.split('')
+    const wordHidden = wordAsArray.map((l)=>l = '-')
+    return wordHidden
+    }
+
+
+newRandomWord
+    useEffect(() => {
+    newRandomWord()
+      setResponseToDisplay(hideResponse(randomWord))
+    }, [])
+
+
+
+
+
 
   return <div className="h-1/5 w-full flex items-center justify-center" onLoadStart={newRandomWord}>
     <p className="text-center text-7xl uppercase">{responseToDisplay}</p>
